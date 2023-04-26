b714287db50d6fa6468769317662eb8362d016c2
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-04-27 01:22:41 +0330

    added paginations and home page

--- app/src/pages/Blog/BlogList.jsx
+++ app/src/pages/Blog/BlogList.jsx
@@ -10,21 +10,32 @@ const BlogList = () => {
   const [search, setSearch] = useState("");
   const [order, setOrder] = useState("");
 
+  const [page, setPage] = useState(1);
+  const [total_page, setTotalPage] = useState(1);
+  const [page_size, setPageSize] = useState(5);
+
   const getData = () => {
     axios
       .get("https://fastapi-blog.iran.liara.run/blog/api/v1/post/", {
         timeout: 5000,
-        params: { search: search,ordering:order },
+        params: {
+          search: search,
+          ordering: order,
+          page: page,
+          page_size: page_size,
+        },
       })
       .then((response) => {
         // console.log(response);
         setBlogs(response.data.results);
+        setPage(response.data.page);
+        setTotalPage(response.data.total_pages);
       })
       .catch((err) => console.log(err));
   };
   useEffect(() => {
     getData();
-  }, [search,order]);
+  }, [search, order, page, page_size]);
 
   return (
     <>
@@ -33,14 +44,31 @@ const BlogList = () => {
         <div className="input-group mb-3">
           <input
             type="text"
-            className="form-control w-75"
-            placeholder="search current page"
+            className="form-control w-50"
+            placeholder="search posts"
             onChange={(e) => setSearch(e.target.value)}
           />
-          <select className="form-select w-25" onChange={(e)=>setOrder(e.target.value)} >
-            <option defaultValue>OrderBy</option>
+          <select
+            className="form-select w-25"
+            onChange={(e) => setOrder(e.target.value)}
+          >
+            <option defaultValue value="">
+              OrderBy (default)
+            </option>
             <option value="id">ID Ascending</option>
             <option value="-id">ID Descending</option>
+          </select>
+          <select
+            className="form-select w-25"
+            onChange={(e) => setPageSize(e.target.value)}
+          >
+            <option defaultValue value="5">
+              page size (default)
+            </option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
           </select>
         </div>
         <div className="row mb-2">
@@ -58,6 +86,66 @@ const BlogList = () => {
                 </div>
               </div>
             ))}
+        </div>
+        <div className="d-flex justify-content-center">
+          <nav aria-label="Page navigation">
+            <ul className="pagination">
+              {total_page > 1 && page - 1 != 0 && (
+                <li className="page-item">
+                  <button className="page-link" onClick={() => setPage(1)}>
+                    first
+                  </button>
+                </li>
+              )}
+              {total_page > 1 && page - 1 != 0 && (
+                <li className="page-item">
+                  <button
+                    className="page-link"
+                    onClick={() => setPage(page - 1)}
+                  >
+                    <i className="bi bi-arrow-left"></i>
+                  </button>
+                </li>
+              )}
+
+              {total_page &&
+                [...Array(total_page).keys()].map((i) => (
+                  <li
+                    className={
+                      i + 1 == page ? " page-item active" : "page-item"
+                    }
+                    key={i + 1}
+                  >
+                    <button
+                      className="page-link"
+                      onClick={() => setPage(i + 1)}
+                    >
+                      {i + 1}
+                    </button>
+                  </li>
+                ))}
+              {total_page > 1 && page < total_page && (
+                <li className="page-item">
+                  <button
+                    className="page-link"
+                    onClick={() => setPage(page + 1)}
+                  >
+                    <i className="bi bi-arrow-right"></i>
+                  </button>
+                </li>
+              )}
+              {total_page > 1 && page < total_page && (
+                <li className="page-item">
+                  <button
+                    className="page-link"
+                    onClick={() => setPage(total_page)}
+                  >
+                  last
+                  </button>
+                </li>
+              )}
+            </ul>
+          </nav>
         </div>
       </div>
       <Footer />
--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
-    <Header />
-    <div>Home</div>
-    <Footer/>
-    </>
-  )
-}
+      <Header />
+      <div className="container px-4 py-5" id="Author">
+        {/* <h2 className="pb-2 border-bottom">Author</h2> */}
+        <div className="row featurette py-5">
+          <div className="col-md-8 order-md-2">
+            <h2 className="featurette-heading">
+              Ali Bigdeli{" "}
+              <span className="text-muted">IOT and AI Developer</span>
+            </h2>
+            <p className="lead">
+              Ali Bigdeli is an IT graduated and with a degree in artificial
+              intelligence and robotics. His interest in the world of artificial
+              intelligence and communications in the world of IoT led him to
+              combine the two platforms. As the basis of his education, work,
+              and development.
+            </p>
 
-export default Home
+            <p className="lead">
+              His more than 5 years of programming experience and ability in the
+              field of networking and communications facilitated his work
+              process and plays a key role in many projects. Python, C ++ / C,
+              and Arduino are his specialties in programming.
+            </p>
+
+            <p className="lead">
+              Since 2016, he has founded a team called ICC-ARIA, whose goals are
+              to teach various specialties in the field of artificial
+              intelligence and the Internet of Things, in which backend and
+              framework designs such as Django play an important role. Ideation
+              and conversion of an idea into a product and reverse engineering
+              of products are among his potential abilities.
+            </p>
+          </div>
+          <div className="col-md-4 order-md-1">
+            <img
+              src="https://avatars.githubusercontent.com/u/29748439?v=4"
+              width="100%"
+              height="100%"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
