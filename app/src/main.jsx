69db32a7303d800815327b2eab218908f0aef9f0
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-05-05 00:23:50 +0330

    added support for env

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -11,13 +11,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
+const debug = import.meta.env.VITE_DEBUG;
 
+console.log(debug);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
+        {debug == "true" && <ReactQueryDevtools />}
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 export const api = axios.create({
-  baseURL: "https://fastapi-blog.iran.liara.run",
+  baseURL: import.meta.env.VITE_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
